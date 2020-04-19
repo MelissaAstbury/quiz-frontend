7ac0c0df8bcd3cc98719fe09a4d434fdd214356e
Melissa Astbury <melissajastbury@gmail.com>
2020-04-19 16:16:09 +0100

    added reat route to the project and set this up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import "./App.css";
+
+import Home from "./containers/Home/Home";
+import Quiz from "./containers/Quiz/Quiz";
 
 function App() {
   return (
-    <div>
-      <p>mong</p>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/quiz" component={Quiz} />
+    </Switch>
   );
 }
 
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Quiz.scss";
+
+const Quiz = () => {
+  return (
+    <div>
+      <h1>Question One</h1>
+    </div>
+  );
+};
+
+export default Quiz;
